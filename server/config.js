4fd74c116d38edcf4d71c7bd80deb92a3d3a402c
Rahul Varal <rahul.varal@magicedtech.com>
2023-07-17 12:35:42 +0530

    Created API's

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "<secret>",
+    EMAIL: "tomasa.barton@ethereal.email", // testing email & password
+    PASSWORD : "FkegJHkt4PCJXcPbvb",
+    ATLAS_URI: "<MONGODB_ATLAS_URI>"
+}
